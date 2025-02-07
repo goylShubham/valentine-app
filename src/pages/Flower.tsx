2ff262c6d0bcb3f01a141ec729893651a2bc8b3d
Shubham Goyal <goylshubham@gmail.com>
2025-02-07 09:50:31 +0530

    update email

--- src/pages/Flower.tsx
+++ src/pages/Flower.tsx
@@ -3,26 +3,29 @@ import { useContext } from "react";
 import { useEffect } from "react";
 import { AppContext } from "../App";
 
-
-
-
 const Flower = ({ nextPage }) => {
-  const {state} = useContext(AppContext)
+  const { state } = useContext(AppContext);
 
   useEffect(() => {
     const sendEmail = async () => {
-      const formData = new FormData();
-      formData.append("email", "goylshubham@gmail.com"); // Replace with your email
-      formData.append("message", JSON.stringify(state));
-    
       try {
-        const response = await fetch("https://formsubmit.co/73d3760b6444adc5265548f7b58500ce", {
-          method: "POST",
-          body: formData,
-        });
-    
+        const response = await fetch(
+          "https://formsubmit.co/ajax/goylshubham@gmail.com",
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+              Accept: "application/json",
+            },
+            body: JSON.stringify({
+              name: "The Valentine App",
+              message: JSON.stringify(state),
+            }),
+          }
+        );
+
         if (response.ok) {
-          console.log("Email sent successfully!");
+          console.log("Email sent successfully.");
         } else {
           console.log("Failed to send email.");
         }
@@ -30,57 +33,59 @@ const Flower = ({ nextPage }) => {
         console.error("Error:", error);
       }
     };
-  
-    sendEmail()
-  }, [])
-  
+
+    sendEmail();
+  }, []);
+
   return (
     <div className="flower-root">
-    <div id="thankyou"><b>Thank you for being my girlfriend</b></div>
-    <div className="flower">
-      <div className="f-wrapper">
-        <div className="flower__line"></div>
-        <div className="f">
-          <div className="flower__leaf flower__leaf--1"></div>
-          <div className="flower__leaf flower__leaf--2"></div>
-          <div className="flower__leaf flower__leaf--3"></div>
-          <div className="flower__leaf flower__leaf--4"></div>
-          <div className="flower__leaf flower__leaf--5"></div>
-          <div className="flower__leaf flower__leaf--6"></div>
-          <div className="flower__leaf flower__leaf--7"></div>
-          <div className="flower__leaf flower__leaf--8 flower__fall-down--yellow"></div>
-        </div>
+      <div id="thankyou">
+        <b>Thank you for being my girlfriend</b>
       </div>
+      <div className="flower">
+        <div className="f-wrapper">
+          <div className="flower__line"></div>
+          <div className="f">
+            <div className="flower__leaf flower__leaf--1"></div>
+            <div className="flower__leaf flower__leaf--2"></div>
+            <div className="flower__leaf flower__leaf--3"></div>
+            <div className="flower__leaf flower__leaf--4"></div>
+            <div className="flower__leaf flower__leaf--5"></div>
+            <div className="flower__leaf flower__leaf--6"></div>
+            <div className="flower__leaf flower__leaf--7"></div>
+            <div className="flower__leaf flower__leaf--8 flower__fall-down--yellow"></div>
+          </div>
+        </div>
 
-      <div className="f-wrapper f-wrapper--2">
-        <div className="flower__line"></div>
-        <div className="f">
-          <div className="flower__leaf flower__leaf--1"></div>
-          <div className="flower__leaf flower__leaf--2"></div>
-          <div className="flower__leaf flower__leaf--3"></div>
-          <div className="flower__leaf flower__leaf--4"></div>
-          <div className="flower__leaf flower__leaf--5"></div>
-          <div className="flower__leaf flower__leaf--6"></div>
-          <div className="flower__leaf flower__leaf--7"></div>
-          <div className="flower__leaf flower__leaf--8 flower__fall-down--pink"></div>
+        <div className="f-wrapper f-wrapper--2">
+          <div className="flower__line"></div>
+          <div className="f">
+            <div className="flower__leaf flower__leaf--1"></div>
+            <div className="flower__leaf flower__leaf--2"></div>
+            <div className="flower__leaf flower__leaf--3"></div>
+            <div className="flower__leaf flower__leaf--4"></div>
+            <div className="flower__leaf flower__leaf--5"></div>
+            <div className="flower__leaf flower__leaf--6"></div>
+            <div className="flower__leaf flower__leaf--7"></div>
+            <div className="flower__leaf flower__leaf--8 flower__fall-down--pink"></div>
+          </div>
         </div>
-      </div>
 
-      <div className="f-wrapper f-wrapper--3">
-        <div className="flower__line"></div>
-        <div className="f">
-          <div className="flower__leaf flower__leaf--1"></div>
-          <div className="flower__leaf flower__leaf--2"></div>
-          <div className="flower__leaf flower__leaf--3"></div>
-          <div className="flower__leaf flower__leaf--4"></div>
-          <div className="flower__leaf flower__leaf--5"></div>
-          <div className="flower__leaf flower__leaf--6"></div>
-          <div className="flower__leaf flower__leaf--7"></div>
-          <div className="flower__leaf flower__leaf--8 flower__fall-down--purple"></div>
+        <div className="f-wrapper f-wrapper--3">
+          <div className="flower__line"></div>
+          <div className="f">
+            <div className="flower__leaf flower__leaf--1"></div>
+            <div className="flower__leaf flower__leaf--2"></div>
+            <div className="flower__leaf flower__leaf--3"></div>
+            <div className="flower__leaf flower__leaf--4"></div>
+            <div className="flower__leaf flower__leaf--5"></div>
+            <div className="flower__leaf flower__leaf--6"></div>
+            <div className="flower__leaf flower__leaf--7"></div>
+            <div className="flower__leaf flower__leaf--8 flower__fall-down--purple"></div>
+          </div>
         </div>
+        <div className="flower__glass"></div>
       </div>
-      <div className="flower__glass"></div>
-    </div>
     </div>
   );
 };
